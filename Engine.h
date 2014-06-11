af4ab3a4d6d9deb15f26b97d3d33c322ef4be6d0
Oscar <oswi3035@student.su.se>
2014-06-11 17:58:10 +0200

    Added Engine.c and Engine.h

---
+++ Engine.h
@@ -0,0 +1,24 @@
+//
+//  Engine.h
+//  CTratt2
+//
+//  Created by Oscar Wiberg on 11/06/14.
+//  Copyright (c) 2014 Oscar Wiberg. All rights reserved.
+//
+
+#ifndef CTratt2_Engine_h
+#define CTratt2_Engine_h
+
+typedef struct GameObject GameObject;
+
+typedef struct {
+    float x,y,z;
+} Vec3;
+
+extern float deltaTime;
+extern Vec3 camera;
+
+
+
+
+#endif
